e8cc935ae14af26132ae083bac40c434846d784e
Felipe Serejo Monteiro <125762883+lipe-s-m@users.noreply.github.com>
2023-07-10 18:39:52 -0300

    Add files via upload

--- jogo.c
+++ jogo.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
 #include <string.h>
 #include <ctype.h>
 #include <time.h>
@@ -9,13 +8,29 @@ void capitalize(char str1[], char str2[]);
 unsigned short int strings_iguais(char str1[], char str2[]);
 int credito(int menu1);
 void atributo();
+typedef struct _PERSONAGEM
+	{
+        int vida;
+        int ataque;
+        int velocidade;
+        int defesa;
+	}PERSONAGEM;
 
+PERSONAGEM guerreiro, assassino, mago;
 
+void boneco();
 int main()
 {
 
-    SetConsoleCP(1252);
-    SetConsoleOutputCP(1252);
+   
+    FILE *fp;
+    fp = fopen("oi.text", "r");
+    char o;
+    int u = 0;
+    PERSONAGEM guerreiro, assassino, mago;
+    guerreiro.vida = 4;
+    guerreiro.ataque = 8;    
+    printf("%d, %d", guerreiro.vida, guerreiro.ataque);
 
     int i = 0, d6 = 0, d12 = 0, d20 = 0, menu = 0, sexo = 0, nome_classe = 0;
     int person = 0, classe = 0, forca = 8, magia = 8, destreza = 8, vigor = 8, inteligencia = 8, sorte = 8, carisma = 8, points = 0, xp = 0;
@@ -29,16 +44,17 @@ int main()
 
     printf("\n-------------------------------");
     printf("\n*****Bem Vindo ao Jogo!***** \n \n");
-    printf("Digite 1 para Iniciar um Novo Jogo \nDigite 2 para Carregar um Jogo Salvo \nDigite 3 para ver os Créditos \nDigite 4 para Sair \n\n    >>> ");
+    printf("Digite 1 para Iniciar um Novo Jogo \nDigite 2 para Carregar um Jogo Salvo \nDigite 3 para ver os Cr?ditos \nDigite 4 para Sair \n\n    >>> ");
     scanf("%d", &menu);
 
     switch (menu)
     {
     case 1:
-        system("cls");
-        printf("-------------------------------\nVocê iniciou um Novo Jogo!\nInsira seu Nick\n    >>> ");
+        printf("-------------------------------\nVoc? iniciou um Novo Jogo!\nInsira seu Nick\n    >>> ");
+        system("clear");
+
         fflush(stdin);
-        gets(nome);
+        scanf("%s", nome);
         nome[i - 1] = '\0';
         capitalize(nome, nome);
 
@@ -48,19 +64,18 @@ int main()
         {
             while (sexo != 1 && sexo != 2)
             {
-                printf("\n***Comando Inválido***");
+                printf("\n***Comando Inv?lido***");
                 printf("\n \nDigite 1 para Homem / Digite 2 para Mulher\n\n    >>> ");
                 scanf("%d", &sexo);
             }
         }
 
+        system("clear");
         if (sexo == 1)
             printf("\nBem Vindo ao Jogo, %s!!!\n\n", nome);
         else if (sexo == 2)
             printf("\nBem Vinda ao Jogo, %s!!!\n\n", nome);
 
-        system("pause");
-        system("cls");
 
         printf("\n\nEscolha sua classe, %s!\n1 - Guerreiro / 2 - Assassino / 3 - Mago\n    >>> ", nome);
         fflush(stdin);
@@ -73,11 +88,11 @@ int main()
             {
             case (1):;
                 char classman[20] = "Guerreiro";
-                printf("\nVocê é um %s\n", classman);
+                printf("\nVoc? ? um %s\n", classman);
                 break;
             case (2):;
                 char classwoman[20] = "Guerreira";
-                printf("\nVocê é uma %s\n", classwoman);
+                printf("\nVoc? ? uma %s\n", classwoman);
                 break;
             }
             points = 0;
@@ -97,11 +112,11 @@ int main()
             {
             case (1):;
                 char classman[20] = "Assassino";
-                printf("\nVocê é um %s\n", classman);
+                printf("\nVoc? ? um %s\n", classman);
                 break;
             case (2):;
                 char classwoman[20] = "Assassina";
-                printf("\nVocê é uma %s\n", classwoman);
+                printf("\nVoc? ? uma %s\n", classwoman);
                 break;
             }
             points = 0;
@@ -118,11 +133,11 @@ int main()
             {
             case (1):;
                 char classman[20] = "Mago";
-                printf("\nVocê é um %s\n", classman);
+                printf("\nVoc? ? um %s\n", classman);
                 break;
             case (2):;
                 char classwoman[20] = "Maga";
-                printf("\nVocê é uma %s\n", classwoman);
+                printf("\nVoc? ? uma %s\n", classwoman);
                 break;
             }
             points = 0;
@@ -138,10 +153,23 @@ int main()
         }
         printf("\n\n%d %d %d\n", forca, vigor, magia);
         printf("%s", atributos);
+
+
+
         break;
+
+
+    
+
+
+
+
     case (3):
         system("cls");
         credito(menu);
+        puts("\n");
+        fscanf(fp, "%c", &o);
+        printf("%c", o);
         break;
 
     default:
@@ -158,9 +186,16 @@ void atributo()
     
 }
 int credito(int menu1){
-    printf("\n-------------------------------\nVocê Selecionou a opção Créditos!\n\n");
-    printf("Feito por Felipe Serejo Monteiro, aluno do 1º período de Ciências da Computação \nda UFRRJ de Nova Iguaçu, no ano de 2023.\n");
+    printf("\n-------------------------------\nVoc? Selecionou a op??o Cr?ditos!\n\n");
+    printf("Feito por Felipe Serejo Monteiro, aluno do 1? per?odo de Ci?ncias da Computa??o \nda UFRRJ de Nova Igua?u, no ano de 2023.\n");
     puts("\n");
+    return 0;
+}
+
+void boneco()
+{
+    guerreiro.vida = 4;
+    guerreiro.ataque = 8;
     return 0;
 }
 void capitalize(char str1[], char str2[])
@@ -182,4 +217,3 @@ void capitalize(char str1[], char str2[])
     str2[0] = toupper(str2[0]);
     str2[i] = '\0';
 }
-
